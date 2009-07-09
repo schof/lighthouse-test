04bb1636ac50b8e7c0f52bb93895a3671b41911f
Sean Schofield <schof@apache.org>
2009-07-08 20:39:23 -0400

    Test commit.

--- app.rb
+++ app.rb
@@ -1,3 +1,16 @@
 get '/' do
   'Hello Sinatra!'
 end
+
+post '/' do
+  payload = JSON.parse(params[:payload])
+  #messages = []
+  #repository = payload['repository']
+ 
+  #payload['commits'].each do |sha, commit|
+  #  Campfire.notify("[%s] %s - %s %s" % [ repository['name'], commit['author']['name'], commit['message'], commit['url'] ])
+  #end  
+  #push = JSON.parse(params[:payload])
+  puts ">>> I got some JSON: #{payload.inspect}"
+  "I got some JSON: #{payload.inspect}" 
+end
--- config.ru
+++ config.ru
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'json'
 
 Sinatra::Application.set(
   :run => false,
