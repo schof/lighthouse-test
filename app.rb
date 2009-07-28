3f63ab0214813ec6b0bef7333390d2e5e8b1b169
Sean Schofield <sean@railsdog.com>
2009-07-27 22:19:01 -0400

    Test of patch state.  [#4]

--- app.rb
+++ app.rb
@@ -47,7 +47,9 @@ post '/' do
     project = Lighthouse::Project.find(ENV['LIGHTHOUSE_PROJECT'])
     
     ticket = Lighthouse::Ticket.find(ticket_id, :params => {:project_id => ENV['LIGHTHOUSE_PROJECT']})
-    # TDODO -- determine ticket number, etc.
+    next unless ticket and %w{open reopened hold stalled}.include?(ticket.state)
+    ticket.state = "patched"
+    ticket.save
   end
 
   "Yay!"  
